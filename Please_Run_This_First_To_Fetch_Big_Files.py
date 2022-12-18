21e51f8c2874b6b2205953bb9b8bf7a695359b87
fuqingxu-home <505030475@qq.com>
2022-12-18 16:52:48 +0800

    better download script

--- Please_Run_This_First_To_Fetch_Big_Files.py
+++ Please_Run_This_First_To_Fetch_Big_Files.py
@@ -11,9 +11,10 @@ def download_from_shared_server(key = 'cat'):
     manifest_url = "https://ageasga-my.sharepoint.com/:u:/g/personal/fuqingxu_yiteam_tech/EVmCQMSUWV5MgREWaxiz_GoBalBRV3DWBU3ToSJ5OTQaLQ?e=I8yjl9"
 
     try:
-        file = download(manifest_url, filename="./TEMP/", force_download=True)
+        download(manifest_url, filename="./TEMP/", force_download=True)
     except:
         print('failed to connect to onedrive | 连接onedrive失败, 您可能需要翻墙才能下载资源')
+        return False
 
     with open("./TEMP/uhmap_manifest.jsonc", "r") as f:
         manifest = commentjson.load(f)
@@ -21,18 +22,25 @@ def download_from_shared_server(key = 'cat'):
     uhmap_url = manifest[key]
     print('download main files | 下载预定文件')
     try:
-        file = download(uhmap_url, filename="./TEMP/DOWNLOAD", unzip=True, unzip_path='./TEMP/UNZIP')
+        download(uhmap_url, filename="./TEMP/DOWNLOAD", unzip=True, unzip_path='./TEMP/UNZIP')
     except:
         print(f'download timeout | 下载失败, 您可能需要翻墙才能下载资源。另外如果您想手动下载的话: {uhmap_url}')
-    return file
+        return False
+
+    return True
 
 def get_current_version():
     with open('current_version', 'r', encoding='utf8') as f:
         version = f.read()
         return version
-        
-version = get_current_version()
-download_from_shared_server(f'uhmp-big-file-v{version}') 
-dir_util.copy_tree('./TEMP/UNZIP', './')
 
-print('完成! (如果因网络原因下载失败, 删除TEMP文件夹然后再试一次)')
+version = get_current_version()
+success = download_from_shared_server(f'uhmp-big-file-v{version}') 
+
+if success:
+    dir_util.copy_tree('./TEMP/UNZIP', './')
+    print('下载完成')
+else:
+    try: shutil.rmtree('./TEMP')
+    except: pass
+    print('下载失败, 临时文件夹已清除')
