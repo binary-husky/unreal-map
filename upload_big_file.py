737758be73d22197ddb37e1dc7964a217937a748
binary-husky <qingxu.fu@outlook.com>
2023-07-18 17:36:33 +0800

    add new map

--- upload_big_file.py
+++ upload_big_file.py
@@ -51,28 +51,28 @@ def get_current_version():
 
 desired_version = get_current_version()
 
-os.system(f'.\\7-Zip\\7z.exe a -tzip -mx4 ./Build/uhmp-big-file-v{desired_version}.zip  -ir!Content/Model3D   Plugins  7-Zip  UHMP.uproject  EnvDesignTutorial.pptx')
+# os.system(f'.\\7-Zip\\7z.exe a -tzip -mx4 ./Build/uhmp-big-file-v{desired_version}.zip  -ir!Content/Model3D   Plugins  7-Zip  UHMP.uproject  EnvDesignTutorial.pptx')
+# add_file_to_onedrive(
+#     session = session, 
+#     key = f'uhmp-big-file-v{desired_version}', 
+#     path_file_name_local = f'Build/uhmp-big-file-v{desired_version}.zip')
+
+plat = "Linux"
+key = f"Uhmap_{plat}_Build_Version{desired_version}"
+os.system('python BuildLinuxRender.py')
+os.system('python BuildLinuxServer.py')
+os.system(f'.\\7-Zip\\7z.exe a -tzip -mx4 ./Build/{key}.zip  ./Build/LinuxNoEditor   ./Build/LinuxServer')
 add_file_to_onedrive(
     session = session, 
-    key = f'uhmp-big-file-v{desired_version}', 
-    path_file_name_local = f'Build/uhmp-big-file-v{desired_version}.zip')
+    key = key, 
+    path_file_name_local = f'Build/{key}.zip')
 
-# plat = "Linux"
-# key = f"Uhmap_{plat}_Build_Version{desired_version}"
-# os.system('python BuildLinuxRender.py')
-# os.system('python BuildLinuxServer.py')
-# os.system(f'.\\7-Zip\\7z.exe a -tzip -mx4 ./Build/{key}.zip  ./Build/LinuxNoEditor   ./Build/LinuxServer')
-# add_file_to_onedrive(
-#     session = session, 
-#     key = key, 
-#     path_file_name_local = f'Build/{key}.zip')
-
-# plat = "Windows"
-# key = f"Uhmap_{plat}_Build_Version{desired_version}"
-# os.system('python BuildWindowsRender.py')
-# os.system('python BuildWindowsServer.py')
-# os.system(f'.\\7-Zip\\7z.exe a -tzip -mx4 ./Build/{key}.zip  ./Build/WindowsNoEditor   ./Build/WindowsServer')
-# add_file_to_onedrive(
-#     session = session, 
-#     key = key, 
-#     path_file_name_local = f'Build/{key}.zip')
+plat = "Windows"
+key = f"Uhmap_{plat}_Build_Version{desired_version}"
+os.system('python BuildWindowsRender.py')
+os.system('python BuildWindowsServer.py')
+os.system(f'.\\7-Zip\\7z.exe a -tzip -mx4 ./Build/{key}.zip  ./Build/WindowsNoEditor   ./Build/WindowsServer')
+add_file_to_onedrive(
+    session = session, 
+    key = key, 
+    path_file_name_local = f'Build/{key}.zip')
